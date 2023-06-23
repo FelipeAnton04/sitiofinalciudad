43a249e1fcdb17ab5c64b064f1c6612ef2f335a4
Felipe Anton <felipeanton04@gmail.com>
2023-06-23 09:09:20 -0300

    Proyecto Final

--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,40 @@ router.post('/', async (req, res, next) => {
 
   console.log(req.body)
 
+  var nombre1 = req.body.nombre1;
+  var apellido1 = req.body.apellido1;
+  var email1 = req.body.email1;
+  var jugador = req.body.jugador;
+  var pregunta = req.body.pregunta;
+  
+  var obj = {
+    to: 'felipeanton04@gmail.com',
+    subject: 'Contacto desde la web',
+    html: nombre1 + " " + apellido1 + " se contacto a traves de la pagina, con el siguiente email: " + email1 + " y quiere hacerle a " + jugador + " la siguiente pregunta: " + pregunta +""
+  }
+
+  var transporter = nodemailer.createTransport ({
+    host: process.env.SMTP_HOST, 
+    port: process.env.SMTP_PORT,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS
+    }
+  })
+
+  var info = await transporter.sendMail(obj);
+
+  res.render ('index' , {
+    message: 'Tu opinion fue enviada',
+  });
+
+});
+
+
+router.post('/', async (req, res, next) => {
+
+  console.log(req.body)
+
   var nombre = req.body.nombre;
   var apellido = req.body.apellido;
   var email = req.body.email;
@@ -38,7 +72,7 @@ router.post('/', async (req, res, next) => {
 
   var info = await transporter.sendMail(obj);
 
-  res.render ('index' , {
+  res.render ('#' , {
     message: 'Tu opinion fue enviada',
   });
 });
